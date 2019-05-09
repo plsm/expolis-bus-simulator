d40658cb8688b8b1dd22bfc9a577d2801d284b2e
Pedro Mariano <plmariano@fc.ul.pt>
2019-05-09 16:25:23 +0100

    Implemented projection to a plane.

--- position.py
+++ position.py
@@ -1,6 +1,20 @@
 # coding=utf-8
 
 import math
+import pyproj
+
+EARTH_EQUATORIAL_RADIUS = 6378137.0
+EARTH_POLAR_RADIUS = 6356752.3
+
+# Projection used to convert geographic coordinates to a XY plane and vice-versa.
+# For other projections and parameters see https://proj4.org/usage/projections.html
+# and https://proj4.org/operations/projections/index.html.
+PROJECTION = pyproj.Proj (
+    proj = 'ortho',
+    a = EARTH_EQUATORIAL_RADIUS,
+    b = EARTH_POLAR_RADIUS,
+    units = 'm'
+)
 
 # Represents a geographical position with latitude and longitude.
 class Position:
@@ -11,6 +25,13 @@ class Position:
     # Return a string representation to be used with the open routing service.
     def to_ors (self):
         return '{},{}'.format (self.longitude, self.latitude)
+
+    # Performs a projection to a plane and return the XY coordinates
+    def to_2D (self):
+        return PROJECTION (self.longitude, self.latitude)
+
+    def from_2D (self, x, y):
+        self.longitude, self.latitude = PROJECTION (x, y, inverse = True)
 
     def __str__ (self):
         def to_degrees (x):
@@ -28,6 +49,18 @@ class Position:
 
     def __repr__ (self):
         return str ({'lat' : self.latitude, 'lon' : self.longitude})
+
+def from_2D (x, y):
+    (_lambda, _phi) = PROJECTION (x, y, inverse = True)
+    return Position (latitude = _phi, longitude = _lambda)
+
+# Computes the distance between the two points.
+# We use the projection to obtain points in the plane, and then those points are used to compute the distance.
+# The results depends on the projection and on the points.
+def distance (a, b):
+    xya = a.to_2D ()
+    xyb = b.to_2D ()
+    return math.sqrt ((xya [0] - xyb [0]) ** 2 + (xya [1] - xyb [1]) ** 2)
 
 # Geographic position of the Companhia Carris De Ferro De Lisboa, S.A
 BUS_DEPOT = Position (38.715802, -9.235010)
