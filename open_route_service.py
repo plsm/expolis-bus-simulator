1f166eea80944550ee4c77c4250e56c019e8ddd2
Pedro Mariano <plmariano@fc.ul.pt>
2019-05-17 15:53:41 +0100

    Added an exception to represent no path between start and end points.

--- open_route_service.py
+++ open_route_service.py
@@ -5,6 +5,19 @@ import position
 
 API_KEY = '5b3ce3597851110001cf624837aee23f45014e35aba00a8b77e806c1'
 
+class NoPathError (StandardError):
+    def __init__ (self, start, end, status_code, reason):
+        self.start = start
+        self.end = end
+        self.status_code = status_code
+        self.reason = reason
+    def __str__ (self):
+        return 'No path from {} to {}, server returned {} with {}'.format (
+                self.start, self.end, self.status_code, self.reason)
+    def __repr__ (self):
+        return str ({'start': self.start, 'end': self.end})
+
+
 # Returns a list of routes between the start and end positions.
 # Uses the open routing service.  The route consists of a list of Position instances.
 def path (start, end):
@@ -31,4 +44,4 @@ def path (start, end):
             r.append (end)
         return routes
     else:
-        raise (call.status_code, call.reason)
+        raise NoPathError (start, end, call.status_code, call.reason)
